082e55571004a3e0d3e0b1e58f33468c717e5a73
Jan-Petter <Jan-Petter.Daavoy@outlook.com>
2022-07-23 20:43:08 +0200

    Add js function for displaying numbers

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const add = (a,b) => a+b;
+const add = (a, b) => a + b;
 
-const subtract = (a,b) => a-b;
+const subtract = (a, b) => a - b;
 
-const mulitply = (a,b) => a*b;
+const multiply = (a, b) => a * b;
 
-const divide = (a,b) => a/b;
+const divide = (a, b) => a / b;
 
- const operate = (operator,a,b) => operator(a,b);
+const operate = (operator, a, b) => {
+    if (operator == "add") {
+        return add(a, b);
+    } else if (operator == "subtract") {
+        return subtract(a, b);
+    } else if (operator == "multiply") {
+        return multiply(a, b);
+    } else if (operator == "divide") {
+        return divide(a, b);
+    }
+};
+var dispValue = null;
+const display = document.querySelector(".display");
+const numBtn = document.querySelectorAll(".numbers");
+const opBtn = document.querySelectorAll(".operators");
+const clear = () => display.textContent="";
+
+numBtn.forEach((btn) =>{
+    btn.addEventListener("click", function(){
+        display.textContent += btn.value;
+        dispValue = display.textContent;
+    });
+});
+opBtn.forEach((btn) =>{
+    btn.addEventListener("click", function(){
+        clear();
+    });
+});
